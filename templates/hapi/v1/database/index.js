e91be123bcade236284c6db113a1841065f45ce2
Joao Victor (JV) <victoorsp@gmail.com>
2020-12-14 23:09:36 -0300

    refactor: add command pattern to abstract database command execution

--- templates/hapi/v1/database/index.js
+++ templates/hapi/v1/database/index.js
@@ -2,10 +2,14 @@
 
 const neo4j = require('neo4j-driver');
 const config = require('../../config');
+const commandWrapper = require('./command');
+const connectionFactory = require('./connection');
 
-const connection = require('./connection')({
+const connection = connectionFactory({
   client: neo4j,
   config,
 });
 
-module.exports = connection.getConnection();
+module.exports = commandWrapper({
+  connection: connection.getConnection(),
+});
