e3b6d5266459a4c7b4d0593d1819566808428f03
Joao Victor (JV) <victoorsp@gmail.com>
2020-12-14 23:18:20 -0300

    fix: controller index.js

--- templates/hapi/v1/controllers/index.js
+++ templates/hapi/v1/controllers/index.js
@@ -3,9 +3,11 @@
 const adaptersWrapper = require('../adapters');
 const healthCheckController = require('./health-check-controller');
 const config = require('../../config');
+const database = require('../database')
 
 const adapters = adaptersWrapper({
   config,
+  database,
 });
 
 module.exports = {
