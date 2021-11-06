e31143e1308122b476ab5c8c3dfecaca0f097a7f
theVictoor <victoorsp@gmail.com>
2021-11-05 21:54:30 -0300

    chore: remove database

--- templates/hapi/config/index.js
+++ templates/hapi/config/index.js
@@ -3,19 +3,11 @@
 const {
   APPLICATION_NAME = "no-name",
   PORT = 8000,
-  DATABASE_HOST,
-  DATABASE_USER,
-  DATABASE_PASSWORD
 } = process.env
 
 module.exports = {
   app: {
     name: APPLICATION_NAME,
     port: PORT
-  },
-  database: {
-    connectionHost: DATABASE_HOST,
-    user: DATABASE_USER,
-    password: DATABASE_PASSWORD
   }
 }
--- templates/hapi/v1/controllers/index.js
+++ templates/hapi/v1/controllers/index.js
@@ -3,12 +3,10 @@
 const adaptersWrapper = require('../adapters')
 const healthCheckController = require('./health-check-controller')
 const config = require('../../config')
-const database = require('../database')
 const queries = require('../queries')
 
 const adapters = adaptersWrapper({
   config,
-  database,
   queries
 })
 
